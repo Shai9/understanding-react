994ef9889838786f12cd228a2388df488fb505d4
shai9 <dennis.shakava@student.moringaschool.com>
2022-09-19 14:12:57 +0300

    react info site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactLogo from './images/reactlogo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <nav className='nav'>
+      <img className="image" src= {ReactLogo} alt="React Image" />
+      <ul className='nav-items'>
+            <li>About</li>
+            <li>Pricing</li>
+            <li>Contacts</li>
+          </ul>
+    </nav>
+    <div className='container'>
+        <h1>Why I love React</h1>
+  
+      <ul>
+        <li>Its a hireable skill</li>
+        <li>Used by big companies like facebook</li>
+        <li>Its easy to understand</li>
+        <li>Its a JavaScript Library</li>
+        <li>Used Worldwide by Developers</li>
+      </ul>
+   
+
+      <footer> <small>Copyright 2022 KeDev.</small> </footer>
+    </div>
+    
     </div>
   );
 }
